33bb009ea6ffcbd9c5ca2a3bb5b9988b0b74ca68
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 02:25:25 +0800

    Configure FirebaseApp

--- PhoneNumberLogin/AppDelegate.swift
+++ PhoneNumberLogin/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.rootViewController = ViewController() // FIXME: Replace with REAL root VC
         window.makeKeyAndVisible()
         self.window = window
+
+        FirebaseApp.configure()
+
         return true
     }
 

554c720d96b8e30783595a64c52af5fc731d7661
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 02:08:18 +0800

    Initialize the project (#1)
    * Create a new project
    
    * Remove Main.storyboard

---
+++ PhoneNumberLogin/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  PhoneNumberLogin
+//
+//  Created by Aries Yang on 2019/4/9.
+//  Copyright © 2019 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
+

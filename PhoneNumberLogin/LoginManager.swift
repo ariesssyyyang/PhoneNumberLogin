761d62bed4b1f3f440d9916a5b78517e800db224
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 15:14:49 +0800

    Add new files

---
+++ PhoneNumberLogin/LoginManager.swift
@@ -0,0 +1,9 @@
+//
+//  LoginManager.swift
+//  PhoneNumberLogin
+//
+//  Created by Aries Yang on 2019/4/9.
+//  Copyright © 2019 Aries Yang. All rights reserved.
+//
+
+import Foundation
---
+++ PhoneNumberLogin/Util.swift
@@ -0,0 +1,9 @@
+//
+//  Util.swift
+//  PhoneNumberLogin
+//
+//  Created by Aries Yang on 2019/4/9.
+//  Copyright © 2019 Aries Yang. All rights reserved.
+//
+
+import Foundation

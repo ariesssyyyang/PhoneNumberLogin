53695a77bd8ee0a0d274c06a6dc43ece4cb62f6a
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 15:15:47 +0800

    Implement funtions for using phone number sign-in

--- PhoneNumberLogin/LoginManager.swift
+++ PhoneNumberLogin/LoginManager.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class LoginManager {
+
+    func sendVerificationCode(phoneNumber: String, completion: @escaping (Result<String>) -> Void) {
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let verificationID = verificationID {
+                completion(.success(verificationID))
+            } else {
+                completion(.failure(LoginError.unknown))
+            }
+        }
+    }
+
+    func getCredential(with verificationCode: String, verificationID: String) -> AuthCredential {
+        return PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: verificationCode)
+    }
+
+    func signInUser(with credential: AuthCredential, completion: @escaping (Result<User>) -> Void) {
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let authResult = authResult {
+                completion(.success(authResult.user))
+            } else {
+                completion(.failure(LoginError.unknown))
+            }
+        }
+    }
+}

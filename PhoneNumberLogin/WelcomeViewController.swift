1c41a7f86030c78b3c738306121e70da700646a2
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 20:01:07 +0800

    Minor fix ui of welcome page

--- PhoneNumberLogin/WelcomeViewController.swift
+++ PhoneNumberLogin/WelcomeViewController.swift
@@ -14,12 +14,15 @@ class WelcomeViewController: UIViewController {
     let contentTextView: UITextView = {
         let textView = UITextView()
         textView.text = """
-        😼✌🏽🐷🍸🗿💸
-        👶🏽 GOTCHA! ☺️
-        👻💩👋🏽😈🎉👨🏽‍💻
+        😼 ✌🏽 🐷 🍸 🗿 💸 👽
+        
+        👶🏽  GOT CHA !!!  ☺️
+        
+        👻 💩 👋🏽 😈 🎉 👨🏽‍💻 🦐
         """
         textView.font = UIFont(name: "GillSans-Bold", size: 20)
         textView.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        textView.textAlignment = .center
         textView.isEditable = false
         textView.isUserInteractionEnabled = false
         textView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,14 +31,14 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        view.backgroundColor = .white
         setupWelcomeText()
     }
 
     func setupWelcomeText() {
         view.addSubview(contentTextView)
-        contentTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
-        contentTextView.heightAnchor.constraint(equalTo: contentTextView.widthAnchor).isActive = true
+        contentTextView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        contentTextView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         contentTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         contentTextView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }

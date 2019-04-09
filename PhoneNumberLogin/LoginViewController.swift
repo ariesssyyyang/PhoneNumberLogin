05dce43bad68544ffca1764b0d4f9895b5006dd1
Aries Yang <ariesssyyyang@gmail.com>
2019-04-09 15:46:02 +0800

    Add ui components for login page

--- PhoneNumberLogin/LoginViewController.swift
+++ PhoneNumberLogin/LoginViewController.swift
@@ -8,5 +8,56 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {}
+class LoginViewController: UIViewController {
 
+    let baseView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let phoneNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Phone Number"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let phoneNumTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Please Enter your phone number"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let verifyPhoneNumberButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Send verification code", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let verificationCodeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Verification Code"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let verificationCodeTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Please enter the code from SMS message"
+        textField.textContentType = .oneTimeCode
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign-In", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+}
